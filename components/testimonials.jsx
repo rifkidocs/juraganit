7851ebd58a746fd5cd63ca6a235f67c52a05c46b
Muhammad Rifki Ardiansah <rifkiars@gmail.com>
2025-03-17 13:13:26 +0700

    ui: remove platform icon
    Signed-off-by: Muhammad Rifki Ardiansah <rifkiars@gmail.com>

--- components/testimonials.jsx
+++ components/testimonials.jsx
@@ -2,20 +2,6 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import { Marquee } from "./magicui/marquee";
 
-const PlatformIcon = ({ platform }) => {
-  const iconClasses = "w-4 h-4 text-gray-400";
-
-  return (
-    <div className='absolute top-4 right-4'>
-      {platform === "linkedin" && (
-        <svg className={iconClasses} viewBox='0 0 24 24' fill='currentColor'>
-          <path d='M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z' />
-        </svg>
-      )}
-    </div>
-  );
-};
-
 const ReviewCard = ({ name, username, body, platform }) => {
   return (
     <figure
@@ -25,7 +11,6 @@ const ReviewCard = ({ name, username, body, platform }) => {
         "shadow-[0_-12px_25px_-12px_rgba(0,0,0,0.8)]",
         "shadow-[0_12px_25px_-12px_rgba(0,0,0,0.8)]"
       )}>
-      <PlatformIcon platform={platform} />
       <div className='flex flex-col'>
         <figcaption className='text-base font-semibold text-white'>
           {name}
